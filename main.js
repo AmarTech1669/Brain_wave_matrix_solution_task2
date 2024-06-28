d76ad3d68f193e306eafee7510b301c26f33795b
Amar Banik <amarbanik1669@gmail.com>
2024-06-28 08:16:53 +0600

    small Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
-const navItems = document.querySelector('.nav_items');
+const navItems = document.querySelector('.nav__items');
+const openNavBtn = document.querySelector('#open__nav-btn');
+const closeNavBtn = document.querySelector('#close__nav-btn');
 
-const openNavBtn = document.querySelector('#open_nav-btn');
-
-const closeNavBtn = document.querySelector('#close_nav-btn');
-
-// opens nav dropdown
-
+// Opens nav dropdown
 const openNav = () => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+};
 
-navItems.style.display = 'flex';
-
-openNavBtn.style.display = 'none';
-
-closeNavBtn.style.display = 'inline-block';
-
-}
-
-// close nav dropdown
-
+// Closes nav dropdown
 const closeNav = () => {
-
-navItems.style.display = 'none';
-
-openNavBtn.style.display = 'inline-block';
-
-closeNavBtn.style.display = 'none';
-
-}
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+};
 
 openNavBtn.addEventListener('click', openNav);
-
 closeNavBtn.addEventListener('click', closeNav);
